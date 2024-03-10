08d2f00c9f816ac1f479b58a9fbfeb74c662f7eb
Jack Yang <yangshuosdu@gmail.com>
2024-03-10 17:32:53 +0800

    init

---
+++ code/src/test/java/yangshuosdu/chargingstation/ChargingStationApplicationTests.java
@@ -0,0 +1,13 @@
+package yangshuosdu.chargingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
